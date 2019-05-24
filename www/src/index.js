9cf9b0298b18381d9c82bc536b257cb6469ddc5e
soofm <victorlee228@gmail.com>
2019-05-23 21:35:58 -0400

    Moving some more stuff around

--- www/src/index.js
+++ www/src/index.js
@@ -1,2 +1,4 @@
+import "@/styles/game.css";
+
 import("./bindings.js")
     .catch(e => console.error("Error importing wasm:", e));
--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,11 +2,24 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
-const public = path.resolve(__dirname, "public");
 const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, "src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
   output: {
     path: dist,
     filename: "[name].bundle.js",
@@ -14,12 +27,11 @@ module.exports = {
   devServer: {
       contentBase: dist
   },
-  mode: "development",
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-        template: path.resolve(public, "index.html"),
-        favicon: path.resolve(public, "favicon.ico")
+        template: path.resolve(__dirname, "public", "index.html"),
+        favicon: path.resolve(__dirname, "public", "favicon.ico")
     })
   ],
 };
